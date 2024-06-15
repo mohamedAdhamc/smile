4c6a8baab738daa03cc6f0a1ce0f24c283f50c1f
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2024-06-15 04:30:33 +0300

    add basic UI elements

--- src/UI/MainWindow.py
+++ src/UI/MainWindow.py
@@ -8,3 +8,20 @@ class MainWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="smile")
         self.set_default_size(700, 700)
+
+        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        #add drawing area
+        self.drawingArea = Gtk.DrawingArea()
+        
+        self.vbox.pack_start(self.drawingArea, True, True, 0)
+
+        #add tool buttons in a box at the bottom
+        self.toolBox = Gtk.Box(spacing=0)
+        self.pictureButton = Gtk.Button(label="Picture")
+        self.videoButton = Gtk.Button(label="Video")
+        self.toolBox.pack_start(self.pictureButton, True, True, 0)
+        self.toolBox.pack_start(self.videoButton, True, True, 0)    
+        
+        self.vbox.pack_start(self.toolBox, False, True, 0)    
+
+        self.add(self.vbox)
